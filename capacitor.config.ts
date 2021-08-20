060ff7ac7bbb9a25e28c421697438cb96ebf4a79
Luis <uwu.luishernandez@gmail.com>
2021-08-20 01:50:31 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '100tifi.co-Mobile',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
